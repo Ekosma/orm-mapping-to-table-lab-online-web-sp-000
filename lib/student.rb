89a068fa9521ee2de88ebbb1381ddc4af6a55e0d
Evelyn Kosma <evelyn.a.kosma@gmail.com>
2021-05-30 20:32:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,29 @@ class Student
     @grade = grade
   end
  
-  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end 
+ 
+  def self.create(name:, grade:)
+   student = Student(name, grade)
+   student.save
+   student
+  end
+ 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
   
 end
